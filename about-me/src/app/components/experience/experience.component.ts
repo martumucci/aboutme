0e5954d5791daadb05739c0254f1cd9fcc6f263c
mmucci002 <martina.m.mucci@pwc.com>
2024-01-14 17:57:42 -0300

    added functionality to open learnmore modal

--- about-me/src/app/components/experience/experience.component.ts
+++ about-me/src/app/components/experience/experience.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { LearnMoreComponent } from './learn-more/learn-more.component';
 
 @Component({
   selector: 'app-experience',
   standalone: true,
-  imports: [],
+  imports: [LearnMoreComponent],
   templateUrl: './experience.component.html',
   styleUrl: './experience.component.scss'
 })
 export class ExperienceComponent {
+  open: boolean = false;
+
+  openModal() {
+    this.open = true;
+  }
+
+  closeModal(value: boolean){
+    this.open = value;
+  }
 
 }
--- about-me/src/app/components/experience/learn-more/learn-more.component.ts
+++ about-me/src/app/components/experience/learn-more/learn-more.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, EventEmitter, HostListener, Input, Output, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-learn-more',
@@ -8,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrl: './learn-more.component.scss'
 })
 export class LearnMoreComponent {
+  @ViewChild('learnMore', { static: true }) dialog!: ElementRef<HTMLDialogElement>;
+  @Input() set open(value: boolean) {
+    if (value) {
+      this.dialog.nativeElement.showModal();
+    }
+  }
+  @Output() isOpen = new EventEmitter<boolean>();
+
+  @HostListener('window:keydown.escape', ['$event']) handleEscape(event: KeyboardEvent) {
+    this.close();
+  }
+
+  close() {
+    this.dialog.nativeElement.close();
+    this.isOpen.emit(false);
+  }
 
 }
